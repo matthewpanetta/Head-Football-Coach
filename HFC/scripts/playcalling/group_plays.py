b2410a7dc1176d0c9da9048f4440dcf802e6037c
Thomas Kennedy <tkennedy@smu.edu>
2022-09-24 23:11:50 -0500

    Some playcall analytics data tweak

--- HFC/scripts/playcalling/group_plays.py
+++ HFC/scripts/playcalling/group_plays.py
@@ -5,10 +5,13 @@ import zipfile
 
 def serialize_play(play):
 
+    if play['down'] == 'NA':
+        return None
+
     quarter_seconds_remaining_desc = ''
     play['quarter_seconds_remaining'] = int(play['quarter_seconds_remaining'])
     if play['qtr'] == '1':
-        quarter_seconds_remaining_desc = '1st_qtr'
+        quarter_seconds_remaining_desc = 'any_time'
     elif play['qtr'] == '2':
         if play['quarter_seconds_remaining'] <= 60:
             quarter_seconds_remaining_desc = 'last_min'
@@ -36,14 +39,22 @@ def serialize_play(play):
     score_diff_desc = ''
     if play['score_differential'] == 'NA':
         score_diff_desc = 'NA'
-    elif int(play['score_differential']) > 20:
-        score_diff_desc = 'big_leading'
+    elif int(play['score_differential']) >= 25:
+        score_diff_desc = 'leading_4_score'
+    elif int(play['score_differential']) >= 17:
+        score_diff_desc = 'leading_3_score'
+    elif int(play['score_differential']) >= 9:
+        score_diff_desc = 'leading_2_score'
     elif int(play['score_differential']) > 0:
-        score_diff_desc = 'leading'
-    elif int(play['score_differential']) < -20:
-        score_diff_desc = 'big_trailing'
+        score_diff_desc = 'leading_1_score'
+    elif int(play['score_differential']) <= -25:
+        score_diff_desc = 'trailing_4_score'
+    elif int(play['score_differential']) <= -17:
+        score_diff_desc = 'trailing_3_score'
+    elif int(play['score_differential']) <= -9:
+        score_diff_desc = 'trailing_2_score'
     elif int(play['score_differential']) < 0:
-        score_diff_desc = 'trailing'
+        score_diff_desc = 'trailing_1_score'
     else:
         score_diff_desc = 'tie'
 
@@ -64,8 +75,12 @@ def serialize_play(play):
         yard_desc = 'own_40'
     elif int(play['yardline_100']) <= 70:
         yard_desc = 'own_30'
-    elif int(play['yardline_100']) <= 100:
+    elif int(play['yardline_100']) <= 80:
+        yard_desc = 'own_20'
+    elif int(play['yardline_100']) <= 90:
         yard_desc = 'own_10'
+    elif int(play['yardline_100']) <= 100:
+        yard_desc = 'own_goalline'
     else:
         yard_desc = 'NA'
 
@@ -81,10 +96,10 @@ def serialize_play(play):
     else:
         yards_to_go_desc = 'long'
 
-    qtr_desc = 'OT' if int(play['qtr']) > 4 else play['qtr']
+    qtr_desc = 'OT' if int(play['qtr']) > 4 else 'q'+play['qtr']
 
     return '|'.join([
-        qtr_desc, quarter_seconds_remaining_desc, play['down'], yards_to_go_desc, yard_desc, score_diff_desc
+        qtr_desc, quarter_seconds_remaining_desc, 'd'+play['down'], yards_to_go_desc, yard_desc, score_diff_desc
     ])
 
 
@@ -92,16 +107,30 @@ def group_data(plays):
     grouped_data = {}
     for play in plays:
         serial_play = serialize_play(play)
+        if serial_play is None:
+            continue
         if serial_play not in grouped_data:
-            grouped_data[serial_play] = {}
+            grouped_data[serial_play] = {'total':0}
 
         if play['play_type'] not in grouped_data[serial_play]:
             grouped_data[serial_play][play['play_type']] = 0
 
         grouped_data[serial_play][play['play_type']] += 1
+        grouped_data[serial_play]['total'] += 1
 
-    grouped_data = {k: v for k, v in sorted(grouped_data.items(), key=lambda item: (item[1].get(
-        'run', 0) + item[1].get('pass', 0) + item[1].get('kickoff', 0) + item[1].get('punt', 0), item[0]), reverse=True)}
+    grouped_data = {k: v for k, v in sorted(grouped_data.items(), key=lambda item: (
+        #item[1].get('run', 0) + item[1].get('pass', 0) + item[1].get('kickoff', 0) + item[1].get('punt', 0),
+        #Comment above line to sort just by play situation
+        item[0]), reverse=True)}
+
+    for serial_key, play_count_obj in grouped_data.items():
+        for play_type, play_count in  play_count_obj.items():
+            if play_type == 'total':
+                continue
+            grouped_data[serial_key][play_type] = round(100.0 * grouped_data[serial_key][play_type] / grouped_data[serial_key]['total'], 1)
+
+        # del play_count_obj['total']
+        #Comment above line to keep 'total' field in output
 
     w = open(os.path.join(os.path.dirname(
         sys.argv[0]), 'play_results.json'), 'w')
