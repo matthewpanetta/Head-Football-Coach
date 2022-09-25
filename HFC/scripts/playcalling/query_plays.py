945adef5186712c17e1ac7f244178903441f6a7f
Thomas Kennedy <tkennedy@smu.edu>
2022-09-25 09:27:41 -0500

    Playcall querying

--- HFC/scripts/playcalling/query_plays.py
+++ HFC/scripts/playcalling/query_plays.py
@@ -18,7 +18,8 @@ QUERY_CONFIG = {
 for filter_name, filter_list in QUERY_CONFIG['filters'].items():
     QUERY_CONFIG['filters'][filter_name] = set(filter_list)
 
-def de_serialize_play(serial, playcall_obj = {}):
+
+def de_serialize_play(serial, playcall_obj={}):
 
     serial_split = serial.split('|')
     return {
@@ -32,6 +33,7 @@ def de_serialize_play(serial, playcall_obj = {}):
         'playcalling': playcall_obj
     }
 
+
 with open(os.path.join(os.path.dirname(sys.argv[0]), 'play_results.json'), 'r') as r:
     data = json.load(r)
     r.close()
@@ -42,15 +44,19 @@ with open(os.path.join(os.path.dirname(sys.argv[0]), 'play_results.json'), 'r') 
         if len(filter_list) == 0:
             continue
 
-        verbose_data = [row for row in verbose_data if row.get(filter_name) in filter_list]
+        verbose_data = [row for row in verbose_data if row.get(
+            filter_name) in filter_list]
 
     if (len(QUERY_CONFIG['sorting']) > 0):
-        verbose_data = sorted(verbose_data, key=lambda row: [row[sort_key] for sort_key in QUERY_CONFIG['sorting']])
+        verbose_data = sorted(verbose_data, key=lambda row: [
+                              row[sort_key] for sort_key in QUERY_CONFIG['sorting']])
 
-    
+    for filter_name, filter_list in QUERY_CONFIG['filters'].items():
+        QUERY_CONFIG['filters'][filter_name] = list(filter_list)
+
+    QUERY_CONFIG['data'] = verbose_data
+
     w = open(os.path.join(os.path.dirname(
         sys.argv[0]), 'query_results.json'), 'w')
-    w.write(json.dumps(verbose_data, indent=2))
+    w.write(json.dumps(QUERY_CONFIG, indent=2))
     w.close()
-
-
